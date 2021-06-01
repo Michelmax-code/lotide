0fa5a9bc8dc849657aed315316e8ae6ffe4ece49
Miguel Jimenez <migang9@gmail.com>
2021-05-31 20:36:45 -0400

    AssArrEqualTest

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+//const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+// test code
+assertArraysEqual([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]);
